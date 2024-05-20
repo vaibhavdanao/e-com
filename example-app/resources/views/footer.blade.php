9408de075d9c187e43bd59b9bd205c0d4c8f1eb0
vaibhavdanao06 <vaibhavdanao06@gmail.com>
2024-05-20 13:23:46 +0530

    login page done

---
+++ example-app/resources/views/footer.blade.php
@@ -0,0 +1,16 @@
+<div class="col-md-4">
+                <h5>Follow Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Facebook</a></li>
+                    <li><a href="#" class="text-white">Twitter</a></li>
+                    <li><a href="#" class="text-white">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-12 text-center">
+                <p>&copy; 2024 Your Company. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
